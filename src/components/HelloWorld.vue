5a706dcddd8fb07788bd87472d8b88e22409bd67
xieyankun <kiroader@gmail.com>
2018-06-11 06:55:06 +0800

    feat: 配置sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,19 +95,23 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style scoped lang="scss">
+.hello{
+  h1, h2 {
+    font-weight: normal;
+    color: red;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
